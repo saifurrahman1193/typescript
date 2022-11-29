7015bb81132e90c81db38e067bac75002bbb10d0
saifurrahman1193 <saifur.rahman1193@gmail.com>
2022-11-29 23:56:22 +0600

    types

---
+++ 4.type/output/script.js
@@ -0,0 +1,20 @@
+"use strict";
+let person;
+person = 34;
+console.log(person);
+person = "saifur"; // can change type
+console.log(person);
+const multiply = (a, b) => {
+    return a * b;
+};
+console.log(multiply(3, 4));
+let arr = [0, '']; // only can add numbers, string 
+arr.push(1);
+// arr.push({"a": 1}) // can't add object
+// arr.push(true) // can't add boolean
+console.log(arr);
+let obj = {
+    name: 'saifur',
+    profession: 'software development',
+};
+// obj.country = 'bangladesh'; // Property 'country' does not exist on type '{ name: string; profession: string; }'
